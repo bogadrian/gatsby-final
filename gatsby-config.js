e1a267de699701bd31dab4caee09b73d12abb452
Bogdan Adrian <bogdan4adrian@gmail.com>
2020-03-02 19:55:32 +0100

    e

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
       options: {
         plugins: [
           `gatsby-remark-relative-images`,
+          `gatsby-remark-emoji`, // <-- this line adds emoji
           {
             resolve: `gatsby-remark-images`,
             options: {
@@ -65,14 +66,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-emoji`, // <-- this line adds emoji
-        ],
-      },
-    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
