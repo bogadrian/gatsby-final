43f29217b02c82baf3b5e03ecc1dcb8cfc796a43
Bogdan Adrian <adrian.bogdan@cms-cmno.com>
2021-04-21 22:40:40 +0300

    w

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,9 +38,9 @@ module.exports = {
           {
             resolve: "gatsby-remark-embed-video",
             options: {
-              width: 800,
+              // width: 800,
               ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
+              //  height: 400, // Optional: Overrides optional.ratio
               related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
               noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
               loadingStrategy: "lazy", //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
